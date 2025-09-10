9c082f83deed15e5f4317ac74a9c77805dfafb0a
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2025-09-10 16:48:35 -0600

    feat(entities): create Role and User interfaces
        - Added 'Role' interface with id, name, description, and timestamps.
        - Added 'User' interface with authentication, profile, and status fields.
        - Created index file to export both interfaces for centralized access.

---
+++ src/domain/entities/user.entity.ts
@@ -0,0 +1,20 @@
+export interface User {
+  id: string;
+  email: string;
+  password?: string; // null if using only social login
+  firstName?: string;
+  lastName?: string;
+  bio?: string;
+  photoUrl?: string;
+  isEmailVerified: boolean;
+  isActive: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  lastLoginAt?: Date;
+  allowNotifications: boolean;
+  verificationCode?: string;
+  verificationExpires?: Date;
+  resetPasswordCode?: string;
+  resetPasswordExpires?: Date;
+  googleId?: string;
+}
